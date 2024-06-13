6c1bcf7cc11340d33d1a5e07ce1a1723c456163d
luckybutter1011 <luckybutter322982@gmail.com>
2024-06-14 01:17:44 +1000

    Email Reset

--- app/api/auth/reset/route.ts
+++ app/api/auth/reset/route.ts
@@ -53,9 +53,9 @@ export async function POST(request: NextRequest, response: NextResponse) {
               <meta name="viewport" content="width=device-width, initial-scale=1">
               <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
               <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
-              <title>CAFS</title>
+              <title>Reset Password</title>
               <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
-              <link rel="stylesheet" href="./css/main.css" />
+              <link rel="stylesheet" href="https://www.tagmyphotos.com/css/main.css" />
           </head>
 
           <body>
