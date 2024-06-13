a1f4537e0af46f321b033ce3ff7281d462765d4c
luckybutter1011 <luckybutter322982@gmail.com>
2024-06-13 23:34:55 +1000

    reset_email Template

--- app/api/auth/reset/route.ts
+++ app/api/auth/reset/route.ts
@@ -33,7 +33,7 @@ export async function POST(request: NextRequest, response: NextResponse) {
     })
       .setProtectedHeader({ alg: "HS256" })
       .setIssuedAt()
-      .setExpirationTime("5m")
+      .setExpirationTime("10m")
       .sign(getJwtSecretKey());
 
     const verifyURL = `http://localhost:3000/api/auth/reset?token=${token}`;
@@ -42,8 +42,75 @@ export async function POST(request: NextRequest, response: NextResponse) {
       to: email,
       from: "help@tagmyphotos.com",
       subject: "Password Reset",
-      html: `<p>Hello This is the reset link. Please click this and update your password. Have a nice day.</p>
-      <a>${verifyURL}</a>`,
+      html: `<table class="main" width="100%">
+      <tbody>
+        <tr>
+          <td>
+            <table width="100%">
+              <tbody>
+                <tr>
+                  <td class="two-columns">
+                    <table class="column">
+                      <tbody>
+                        <tr>
+                          <td style="padding: 40px 20px 8px">
+                            <a href="https://tagmyphotos.com">
+                              <img src="./image/favicon.png" alt="" width="280" height="60">
+                            </a>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </td>
+                </tr>
+                <tr>
+                </tr>
+              </tbody>
+            </table>
+            <table width="100%">
+              <tbody>
+                <tr>
+                  <td style="padding: 0px 40px 40px;text-align: center; ">
+                    <p style=" font-size:24px; font-family: Causten-Semi-Bold">Reset Password</p>
+                    <div class="styled-text">
+                      <p style="margin: 0px 0px 0px;">Hello, The system noticed that you are requesting the reset the
+                        password:</p>
+                      <p>If you want to reset your password, click the below button.</p>
+                    </div>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            <table width="100%">
+              <tbody>
+                <tr>
+                  <td style="padding: 0px 60px 60px;text-align: center;">
+                    <a href="${verifyURL}" target="_blank" class="button-primary" id="dynamic-url">
+                      Conform</a>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            <table width="100%">
+              <tbody>
+                <tr>
+                  <td style="padding: 0px 60px 60px;">
+                    <div style="text-align: left;" class="styled-text">
+                      <p>Reset Password Link will be destroyed within <span><b>10 minutes.</b></span>
+                      </p>
+                      <p>Regards,<br> Tagmyphotos Team</p>
+                    </div>
+                    <p id="copyright"
+                      style="text-align: center; font-size: 14px; color: #000; font-family: Causten-Regular;">© 2024
+                      Tagmyphotos Team, All rights reserved</p>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </td>
+        </tr>
+      </tbody>
+    </table>`,
     };
 
     sgMail.send(msg);
